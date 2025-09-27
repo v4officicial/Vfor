d5352ca77d1fbccfc8af95d114bc0d6b14189163
v4officicial <v4.officicial@gmail.com>
2025-09-27 20:58:53 +0530

    update

---
+++ services/Portfolio/portfolios.js
@@ -0,0 +1,142 @@
+console.log("connected");
+/*import * as libi from '/libi/lib.js';
+import * as home from '/JS/Home/home_page.js';
+
+// HTML for the card container
+const portfolio_container = `
+ <div class="portfolio-container"></div>
+`;
+
+// Insert the initial container into elements with the "main_main" class
+libi.get_set_all_class("main_main", portfolio_container);
+/*var code_space=`
+<form method="post" action="#">
+          <div>
+            <div id="" class="identify">html:
+            </div>
+            <textarea  id="html" target="preview" rows="10" cols="40" value="">
+              <html>
+                <body>
+                  <!-- <h1 id="cl">hai everyone
+</h1>-->
+                  <div style="background-color:#f1f1f1;padding:15px;">
+                    <h1>Cinque Terre
+                    </h1>
+                    <h3>Resize the browser window
+                    </h3>
+                  </div>
+                  <div style="overflow:auto">
+                    <div class="menu">
+                      <div class="menuitem">The Walk
+                      </div>
+                      <div class="menuitem">Transport
+                      </div>
+                      <div class="menuitem">History
+                      </div>
+                      <div class="menuitem">Gallery
+                      </div>
+                    </div>
+                    <div class="main">
+                      <h2>The Walk
+                      </h2>
+                      <p>The walk from Monterosso to Riomaggiore will take you approximately two hours, give or take an hour depending on the weather conditions and your physical shape.
+                      </p>
+                      <img src="img_5terre.jpg" style="width:100%">
+                    </div>
+                    <div class="right">
+                      <h2>What?
+                      </h2>
+                      <p>Cinque Terre comprises five villages: Monterosso, Vernazza, Corniglia, Manarola, and Riomaggiore.
+                      </p>
+                      <h2>Where?
+                      </h2>
+                      <p>On the northwest cost of the Italian Riviera, north of the city La Spezia.
+                      </p>
+                      <h2>Price?
+                      </h2>
+                      <p>The Walk is free!
+                      </p>
+                    </div>
+                  </div>
+                  <div style="background-color:#f1f1f1;text-align:center;padding:10px;margin-top:7px;font-size:12px;"> This web page is a part of a demonstration of fluid web design made by w3schools.com. Resize the browser window to see the content respond to the resizing.
+                  </div>
+                <script src="./script.js"></script>
+</body>
+              </html>
+            </textarea>
+            <button type="button" onclick="copy_text('html')" id="copyh">copy
+            </button>
+          </div>
+          <div>
+            <div id="" class="identify">css:
+            </div>
+            <textarea id="css" rows="10" cols="40" value="">
+              <style>
+                /* h1{
+                color:green;
+                }*/
+                /* {
+                  box-sizing: border-box;
+                }
+                .menu {
+                  float: left;
+                  width: 20%;
+                }
+                .menuitem {
+                  padding: 8px;
+                  margin-top: 7px;
+                  border-bottom: 1px solid #f1f1f1;
+                }
+                .main {
+                  float: left;
+                  width: 60%;
+                  padding: 0 20px;
+                  overflow: hidden;
+                }
+                .right {
+                  background-color: lightblue;
+                  float: left;
+                  width: 20%;
+                  padding: 10px 15px;
+                  margin-top: 7px;
+                }
+                @media only screen and (max-width:800px) {
+                  /* For tablets: */
+                  /*.main {
+                    width: 80%;
+                    padding: 0;
+                  }
+                  .right {
+                    width: 100%;
+                  }
+                }
+                @media only screen and (max-width:500px) {
+                  /* For mobile phones: */
+                  /*.menu, .main, .right {
+                    width: 100%;
+                  }
+                }
+              </style>
+            </textarea>
+            <button type="button" onclick="copycss()">copy
+            </button>
+          </div>
+          <div>
+            <div id="" class="identify">js:
+            </div>
+            <textarea id="js" rows="10" cols="40" value="">
+              <script>
+                document.getElementById("cl").innerHTML="red";
+              </script>
+            </textarea>
+            <button type="button" onclick="copyjs()">copy
+            </button>
+          </div>
+          <h4>Customize:
+          </h4>
+          <a href="#view">
+            <input type="button" onclick="display()" value="preview"/>
+          </a>
+        </form>`;*/
+
+//get_set_all_class("rhs",code_spac);
