382aacb5f7d72a63059804c0b4b4a9edddce2be4
v4officicial <v4.officicial@gmail.com>
2025-09-30 14:50:19 +0530

    update

--- services/Jobs/company jobs/primary/primary_job.js
+++ services/Jobs/company jobs/primary/primary_job.js
@@ -64,8 +64,9 @@ fetch('/JSON/Jobs/company_detailes.json')
         </div>
       </section>
        <!--****ad unit start****-->
-       <script async="async" data-cfasync="false" src="//pl27751964.revenuecpmgate.com/b2cd190f5b8c86c5bf4db473bd059b8b/invoke.js"></script>
+       
 <div id="container-b2cd190f5b8c86c5bf4db473bd059b8b"></div>
+<script async="async" data-cfasync="false" src="//pl27751964.revenuecpmgate.com/b2cd190f5b8c86c5bf4db473bd059b8b/invoke.js"></script>
 <!--**** ad unit end ****-->
       <!-- Section 2 (Job Posting Schema) -->
       <section class="section2 lazy-section" itemscope 
@@ -138,7 +139,7 @@ fetch('/JSON/Jobs/company_detailes.json')
       
       
        <!--****ad unit start****-->
-       <script type='text/javascript' src='//pl27751939.revenuecpmgate.com/f5/07/a1/f507a1cda83acce519969d9f0f7d6dc9.js'></script>
+      
  <!--****ad unit end****-->
     `;
 
